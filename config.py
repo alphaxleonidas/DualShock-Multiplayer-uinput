3af2d92d16081ccdaf24dc54245326bb140f3dd1
alphaxleonidas <alphaxleonidas@gmail.com>
2026-05-21 20:36:54 +0500

    Adjust joystick deadzone settings and BT_SWITCH
    Updated deadzone settings for Linux joystick axes and modified BT_SWITCH behavior.

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
 # set to True if you need some debug output in the console
 DEBUG = False
 
-# Deadzone settings, CENTER should be left untouched
-CENTER = 128
-DEADZONE_L = 10  # Left stick
-DEADZONE_R = 10  # Right stick
+# Deadzone settings for Linux joystick axes centered at 0
+CENTER = 0
+DEADZONE_L = 3000   # Left stick
+DEADZONE_R = 3000   # Right stick
 
 # enables or disables the shortcut (PS Button + Start) to disconnect the controller on use
-BT_SWITCH = True
+# Keep this False for a generic DragonRise controller unless you have a real disconnect combo mapped
+BT_SWITCH = False
 
 # change the name of the created virtual controller(s)
 controllerName = "Virtual Uinput Controller"
